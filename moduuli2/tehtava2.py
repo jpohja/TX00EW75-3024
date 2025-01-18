2f37495035aff5e3c398a99fa043f85cf479994b
jpohja <pohja.johanna@gmail.com>
2025-01-18 19:15:53 +0200

    lisäsin moduuli 2 tehtävän 2

--- moduuli2/tehtava2.py
+++ moduuli2/tehtava2.py
@@ -1,4 +1,4 @@
 import math
-ympyran_sade = int(input("Mikä on ympyrän säde? "))
+ympyran_sade = int(input("Anna ympyrän säde: "))
 ympyran_pintaala = math.pi * ympyran_sade ** 2
 print(ympyran_pintaala)
