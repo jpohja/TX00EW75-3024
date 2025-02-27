3ceceedd2a82dac037d4a4d361e56701382bac3c
jpohja <pohja.johanna@gmail.com>
2025-02-27 14:24:39 +0200

    lisäsin moduuli 7 tehtävän 1

---
+++ moduuli7/tehtava1.py
@@ -0,0 +1,5 @@
+
+vuodenajat = ("talvi", "talvi", "kevät", "kevät", "kevät", "kesä", "kesä", "kesä", "syksy", "syksy", "syksy", "talvi")
+kuukauden_numero = int(input("Anna kuukauden numero: "))
+vuodenaika = vuodenajat[kuukauden_numero-1]
+print(f"Vuodenaika on {vuodenaika}.")
